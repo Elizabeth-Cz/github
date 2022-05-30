4876bdf72bf54e13826e21cea4bc852744965820
Elizabeth-Cz <87705292+Elizabeth-Cz@users.noreply.github.com>
2022-05-30 12:07:08 +0200

    Add files via upload
    minor bug fixes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const greenCurry = document.getElementById("green-curry");
-const meatballs = document.getElementById("meatballs");
+const chickenCaesarSalad = document.getElementById("chicken-caesar-salad");
+const grilledCheese = document.getElementById("grilled-cheese");
 const greekSalad = document.getElementById("greek-salad");
 const mushroomPasta = document.getElementById("mushroom-pasta");
-const couscous = document.getElementById("couscous");
-const chickenSalad = document.getElementById("chicken-salad");
-const deselect = document.getElementById("deselect");
+const gyoza = document.getElementById("gyoza");
+const burgers = document.getElementById("burgers");
+const tikkaMasala = document.getElementById("tikka-masala");
+const deselectBtn = document.getElementById("deselect");
 const mealContainer = document.querySelector(".meal__container");
 const scheduleContainer = document.querySelector(".schedule__container");
 
@@ -13,6 +14,7 @@ let selectedColor, active;
 
 mealContainer.addEventListener("click", selectMeal);
 scheduleContainer.addEventListener("click", setColors);
+deselectBtn.addEventListener("click", resetMeals);
 
 // Meal click
 
@@ -21,12 +23,12 @@ function selectMeal(e) {
   mealColor = e.target.style.backgroundColor;
 
   switch (e.target.id) {
-    case "green-curry":
-      activeMeal(greenCurry, mealColor);
+    case "chicken-caesar-salad":
+      activeMeal(chickenCaesarSalad, mealColor);
       text = e.target.innerHTML;
       break;
-    case "meatballs":
-      activeMeal(meatballs, mealColor);
+    case "grilled-cheese":
+      activeMeal(grilledCheese, mealColor);
       text = e.target.innerHTML;
       break;
     case "greek-salad":
@@ -37,33 +39,42 @@ function selectMeal(e) {
       activeMeal(mushroomPasta, mealColor);
       text = e.target.innerHTML;
       break;
-    case "couscous":
-      activeMeal(couscous, mealColor);
+    case "gyoza":
+      activeMeal(gyoza, mealColor);
       text = e.target.innerHTML;
       break;
-    case "chicken-salad":
-      activeMeal(chickenSalad, mealColor);
+    case "burgers":
+      activeMeal(burgers, mealColor);
+      text = e.target.innerHTML;
+      break;
+    case "tikka-masala":
+      activeMeal(tikkaMasala, mealColor);
       text = e.target.innerHTML;
       break;
   }
 }
-document.querySelector("#chicken-salad").addEventListener("click", (e) => {
-  console.log(e);
-  console.log(e.target.innerHTML);
-});
+
 // set color for schedule
 function setColors(e) {
-  if (e.target.classList.contains("meal") && active === true) {
+  if (e.target.style.backgroundColor === selectedColor) {
+    resetColor(e);
+  } else if (e.target.classList.contains("meal") && active === true) {
     e.target.style.backgroundColor = selectedColor;
     e.target.innerHTML = text;
+    console.log(e.target);
   }
+}
+
+function resetColor(e) {
+  e.target.style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+  e.target.innerHTML = "";
+  console.log(e);
 }
 
 // select meal
 
 function activeMeal(meal, color) {
   meal.classList.toggle("selected");
-
   if (meal.classList.contains("selected")) {
     active = true;
     selectedColor = color;
@@ -80,6 +91,7 @@ function resetMeals() {
 
   allMeals.forEach((item) => {
     item.className = "meal__name";
+    active = false;
   });
 }
 
